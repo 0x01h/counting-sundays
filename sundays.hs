df4e7a4b4d720c949e4d5302e200298dffa86f1b
Orçun Özdemir <32897657+n1rv4n4@users.noreply.github.com>
2018-02-25 03:01:50 +0300

    Data type Int changed to Integer & "sundays1" was implemented.

--- sundays.hs
+++ sundays.hs
@@ -1,6 +1,6 @@
-day_of_week :: Int -> Int -> Int -> Int
+day_of_week :: Integer -> Integer -> Integer -> Integer
 day_of_week y m d = z
-  where 
+  where
     z = (d + t1 + k + t2 + t3 + 5 * j) `mod` 7
     j = y `div` 100
     k = y `mod` 100
@@ -10,3 +10,15 @@ day_of_week y m d = z
     m' =
       if m <= 2 then m + 12
       else m + 0
+
+sundays1 :: Integer -> Integer -> Integer
+sundays1 start end = sundays' start 1
+  where
+    sundays' :: Integer -> Integer -> Integer
+    sundays' y m
+      | y > end = 0
+      | otherwise = if day_of_week y m 1 == 1 then rest + 1 else rest
+      where
+        nextY = y + 1
+        nextM = m + 1
+        rest = if (nextM == 13) then sundays' nextY 1 else sundays' y nextM
